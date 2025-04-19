4207f4bcae17f5cf7e58a2a17e4883c67dd47eb1
Jean Santos de Freitas <jsfreitas_jean@hotmail.com>
2025-04-19 16:17:15 -0300

    image optmizer improvement

--- next.config.js
+++ next.config.js
@@ -5,9 +5,6 @@ const nextConfig = {
     output: 'export', // ESSENCIAL para exportação estática
     // Opcional: Desabilita a otimização de imagem via servidor Next.js,
     // já que não teremos um servidor rodando.
-    images: {
-      unoptimized: true,
-    },
   };
   
   module.exports = nextConfig; // ou export default nextConfig; para mjs
